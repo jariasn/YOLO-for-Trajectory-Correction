a28868a6b58bbdeea148e367ae92dc0198a4c605
Joaquin Arias <64325659+jariasn@users.noreply.github.com>
2023-07-19 13:39:03 +0200

    Update lane_detection.py

--- lane_detection.py
+++ lane_detection.py
@@ -44,12 +44,14 @@ def get_boundaries(lines):
         right_lane_extended = np.array([right_x1_extended, y1, right_x2_extended, y2], dtype=np.int32)
     
     return left_lane_extended, right_lane_extended, y1, y2 
+
 def middle_lane(left_lane, right_lane, y1, y2):
     # Calculate the middle line
     middle_x_bottom = int((left_lane[0] + right_lane[0]) / 2)
     middle_x_top = int((left_lane[2] + right_lane[2]) / 2)
     middle_y_bottom = y1
     middle_y_top = y2
+
     return middle_x_bottom, middle_x_top, middle_y_bottom, middle_y_top
 def get_angle(middle_x_bottom, middle_x_top, middle_y_bottom, middle_y_top):
     # Calculate the angle of the middle line
@@ -66,18 +68,48 @@ def get_angle(middle_x_bottom, middle_x_top, middle_y_bottom, middle_y_top):
     # Display the angle on the frame
     cv2.putText(frame, "Angle: {:.2f} degrees".format(deg_off), (20, 40), cv2.FONT_HERSHEY_COMPLEX, 0.6, font_color, 2)
 
+def set_threshold(left_lane, right_lane, threshold, threshold_from_frame, center_of_cam, threshold_x, cam_center_x):
+    if threshold_from_frame:
+        # Set threshold coordinates
+        l_threshold_x_bottom = left_lane[0] + threshold
+        l_threshold_x_top = left_lane[2] + threshold
+
+        r_threshold_x_bottom = right_lane[0] - threshold
+        r_threshold_x_top = right_lane[2] - threshold
+
+        if not center_of_cam:
+            cam_center_x = middle_x_bottom
+
+    else: # If threshold is manually set
+
+        # Angle from y-axis (change to modify the threshold inclination)
+        angle_from_yaxis = 5 
+        angle = np.radians(90 + angle_from_yaxis) 
+
+        # Set threshold coordinates
+        l_threshold_x_bottom = threshold_x - threshold
+        l_threshold_x_top = l_threshold_x_bottom + (y2 - y1) / np.tan(angle)
+
+
+        r_threshold_x_bottom = threshold_x + threshold
+        r_threshold_x_top = r_threshold_x_bottom + (y2 - y1) / np.tan(-angle)
+
+        if not center_of_cam:
+            cam_center_x = threshold_x
+
+    return l_threshold_x_bottom, l_threshold_x_top, r_threshold_x_bottom, r_threshold_x_top, cam_center_x
 # Load a model
-model = YOLO('runs/segment/train/weights/best.pt')  # Load a custom model
+model = YOLO('/Users/joaquinarias/Documents/Jobs/DFKI/RoLand/YOLO/runs/segment/train/weights/best.pt')  # Load a custom model
 
 # Create VideoCapture object to read the input video
-video_capture = cv2.VideoCapture('webcam_lane.mp4')
+video_capture = cv2.VideoCapture('/Users/joaquinarias/Downloads/webcam_lane.mp4')
 
 save = False # Save the video
-threshold_from_frame = True # Take the threshold automatically from frame 
+threshold_from_frame = False # Take the threshold automatically from frame 
 threshold = 50 # Threshold size 
 threshold_x = 300 # x coordinate of threshold
 center_of_cam = False # Use the center of the image as center to calculate displacement
-
+there_is_threshold = None
 
 if save:
     # Get video properties
@@ -92,94 +124,8 @@ if save:
 if center_of_cam:
     cam_width = int(video_capture.get(cv2.CAP_PROP_FRAME_WIDTH))
     cam_center_x = cam_width // 2
-
-while True:
-    ret, frame = video_capture.read()
-    
-    # Break the loop if there are no more frames
-    if not ret:
-        break
-
-    # Predict on the frame
-    results = model(frame)
-    # Get the Masks object from the results
-    masks = results[0].masks if results[0].masks is not None else None 
-    if masks is not None:
-        # Get the mask corresponding to the first detected object (middle)
-        mask = masks.data[0].numpy() 
-
-        # Create a binary image from the mask
-        binary_mask = (mask > 0).astype(np.uint8) * 255
-
-        if threshold_from_frame:
-            # Apply Canny edge detection
-            edges = cv2.Canny(binary_mask, 50, 150)
-
-            # Apply Hough transform to detect lines
-            lines = cv2.HoughLinesP(edges, 1, np.pi / 180, 50, None, 50, 10)
-
-            left_lane, right_lane, y1, y2 = get_boundaries(lines)
-            
-
-            if left_lane is not None and right_lane is not None:
-
-                # Calculate the middle points based on x-coordinates of the lane boundaries
-                middle_x_bottom, middle_x_top, middle_y_bottom, middle_y_top = middle_lane(left_lane, right_lane, y1, y2)
-                # Set threshold coordinates
-                l_threshold_x_bottom = left_lane[0] + threshold
-                l_threshold_x_top = left_lane[2] + threshold
-
-                r_threshold_x_bottom = right_lane[0] - threshold
-                r_threshold_x_top = right_lane[2] - threshold
-
-                # Draw the detected lane boundaries and the middle line on the frame
-                threshold_image = np.zeros_like(frame)
-                
-                # Draw the threshold lines
-                if left_lane is not None:
-                    cv2.line(threshold_image, (l_threshold_x_bottom, middle_y_bottom), (l_threshold_x_top, middle_y_top), (255, 0, 0), thickness=3)
-                if right_lane is not None:
-                    cv2.line(threshold_image, (r_threshold_x_bottom, middle_y_bottom), (r_threshold_x_top, middle_y_top), (255, 0, 0), thickness=3)
-                
-                if not center_of_cam:
-                    cam_center_x = middle_x_bottom
-
-        else: # If threshold is manually set
-            height, width = binary_mask.shape
-            # Angle from y-axis (change to modify the threshold inclination)
-            angle_from_yaxis = 3 
-            angle = np.radians(90 + angle_from_yaxis) 
-            y1 = height - 1  # Bottom of the image
-            y2 = int(height * 0.5)
-
-            # Set threshold coordinates
-            l_threshold_x_bottom = threshold_x - threshold
-            l_threshold_x_top = l_threshold_x_bottom + (y2 - y1) / np.tan(angle)
-
-
-            r_threshold_x_bottom = threshold_x + threshold
-            r_threshold_x_top = r_threshold_x_bottom + (y2 - y1) / np.tan(-angle)
-
-            if not center_of_cam:
-                cam_center_x = threshold_x
-
-            # Draw the detected lane boundaries and the middle line on the frame
-            threshold_image = np.zeros_like(frame)
-
-            y_top = y2
-            y_bottom = y1
-            cv2.line(threshold_image, (int(l_threshold_x_bottom), y_bottom), (int(l_threshold_x_top), y_top), (255, 0, 0), thickness=3)
-            cv2.line(threshold_image, (int(r_threshold_x_bottom), y_bottom), (int(r_threshold_x_top), y_top), (255, 0, 0), thickness=3)
-        
-        break
-    else:
-        if save:
-            output_video.write(frame)
-
-        # Display the resulting frame
-        cv2.imshow('Lane Detection', frame)
-
-        continue
+else:
+    cam_center_x = None
 
 
 while video_capture.isOpened():
@@ -191,7 +137,6 @@ while video_capture.isOpened():
     # Predict with the model
     results = model(frame)
     masks = results[0].masks if results[0].masks is not None else None 
-    
     if masks is not None:
         mask = masks.data[0].numpy()  
 
@@ -210,6 +155,14 @@ while video_capture.isOpened():
         if left_lane is not None and right_lane is not None:
             middle_x_bottom, middle_x_top, middle_y_bottom, middle_y_top = middle_lane(left_lane, right_lane, y1, y2)
 
+            if there_is_threshold is None:
+                l_threshold_x_bottom, l_threshold_x_top, r_threshold_x_bottom, r_threshold_x_top, cam_center_x = set_threshold(left_lane, right_lane, threshold, threshold_from_frame, center_of_cam, threshold_x, cam_center_x)
+            
+            if l_threshold_x_bottom is not None and r_threshold_x_bottom is not None:
+                there_is_threshold = True
+                threshold_image = np.zeros_like(frame)
+                cv2.line(threshold_image, (int(l_threshold_x_bottom), middle_y_bottom), (int(l_threshold_x_top), middle_y_top), (255, 0, 0), thickness=3)
+                cv2.line(threshold_image, (int(r_threshold_x_bottom), middle_y_bottom), (int(r_threshold_x_top), middle_y_top), (255, 0, 0), thickness=3)
             # Get the angle
             get_angle(middle_x_bottom, middle_x_top, middle_y_bottom, middle_y_top)
 
